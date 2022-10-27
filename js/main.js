debb8e2b2ddc95d79dc0fc9ded8c7d79a9e517a0
Michael <michaelgandolfi7@gmail.com>
2022-10-27 16:09:31 -0500

    fixed cors errors

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-document.querySelector("button").addEventListener("click", getInfo) 
+document.querySelector("button").addEventListener("click", apiRequest) 
 
-async function getInfo() {
-    
+async function apiRequest() {
+    const personName = document.querySelector("input").value
+    try {
+        const response = await fetch(`https://simple-family-api.herokuapp.com/api${personName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector("h2").innerText = data.birthName
+    } catch(error) {
+        console.log(error)
+    }
+
 }
